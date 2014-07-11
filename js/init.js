fe151efb96a8e6afafdbb09ed603d902e217f2ba
Kevin Bromer <kbromer@salesforce.com>
2014-07-10 23:52:16 -0400

    Added images, some refactoring, etc.

--- js/init.js
+++ js/init.js
@@ -14,6 +14,36 @@ window.onload = function(){
 		paper.zonecount = 0;
 		var viewport = getViewport();
 		paper.setViewBox(0, 0, w, h, false);
+
+		$("#fighter_img").draggable();
+		$("#fighter_img").draggable("option", "helper", 'clone' );
+
+		$("#armor_img").draggable();
+		$("#armor_img").draggable("option", "helper", 'clone' );
+
+
+		//-------------------- Code for Raphael---------------------
+
+    // Making SVG droppable
+    $("#canvas_container").droppable({
+        drop: function (event, ui) {
+            var svgXY = getSvgCoordinates(event, paper);
+						alert("Window X: " + event.originalEvent.target.x + "Window Y: " + event.originalEvent.target.y + "SVG X: " + svgXY.x + "SVG Y: " + svgXY.y);
+
+						var fighterpathstring = "M" + svgXY.x + "," + svgXY.y + "M`47.713,25.019l5.316-7.519c1.437-2.031,2.559-4.755,2.506-6.084c-0.054-1.33-1.177-2.45-2.511-2.506 c-1.333-0.054-4.06,1.067-6.091,2.504l-7.52,5.318L18.417,6.702c-2.244-1.072-5.196-0.517-6.592,1.243 c-1.396,1.76-0.967,4.463,0.958,6.04l16.784,13.748L17.019,41.635l-5.919-1.787c-2.381-0.721-5.048-0.611-5.954,0.242 c-0.907,0.854-0.07,2.809,1.869,4.368l5.371,4.318c0,0-0.202,0.939-0.453,2.098c-0.25,1.158,0.484,1.89,1.641,1.634l2.094-0.465 l4.315,5.369c1.56,1.939,3.516,2.777,4.37,1.872c0.853-0.905,0.963-3.569,0.242-5.951l-1.788-5.921l13.9-12.548l13.75,16.785 c1.576,1.925,4.28,2.354,6.04,0.957c1.759-1.396,2.316-4.349,1.243-6.594L47.713,25.019z";
+						var newfighter = new Fighter(null, paper);
+						paper.setStart();
+						newfighter.drawElement();
+						paper.setFinish();
+						newfighter.el.attr({transform: ['t', svgXY.x, svgXY.y]});
+						unitMouseupHandler(newfighter.el);
+						///someElement.attr({transform: ['t',100,100]});
+						//newfighter.el.attr({x: svgXY.x, y: svgXY.y});
+						// get targeted SVG elemnet by
+            // event.originalEvent.target
+        }
+    });
+
 //    paper.canvas.setAttribute('preserveAspectRatio', 'none');
 //		var zpd = new RaphaelZPD(paper, { zoom: true, pan: false, drag: false });
 //	  paper.setViewBox(0, 0, 500, 600, true);
@@ -31,37 +61,14 @@ window.onload = function(){
 		paper.zone_set = paper.setFinish();
 
 		//var zonetest = new Zone("m397,105l-72,50l72,73l100,-35l-11,-68l-83,26l14,-32l-20,-14z");
-		paper.setStart();
+/*		paper.setStart();
 		var fighter = new Fighter(null, paper);
 		fighter.drawElement();
 
 				var fighter2 = new Fighter(null, paper);
 				fighter2.drawElement();
 
-		paper.unit_set = paper.setFinish();
-
-//		paper.unit_set.push(fighter2.el);
-
-//paper.unit_set.push(fighter2.el);
-
-/*
-		paper.unit_set.mouseup(function(e) {
-			//use the upper left corner of the element
-			var b = this.getBBox();
-
-			for (var i = 0; i < paper.zone_set.length; i++){
-				var zone_element = paper.zone_set[i];
-
-				if (Raphael.isPointInsidePath(zone_element.attr('path'), b.x, b.y)){
-					paper.zone_set.forEach(function (el) {
-						el.attr({stroke:'black'});
-					});
-					zone_element.attr({stroke: 'red'});
-				}
-			}
-		});
-*/
-
+		paper.unit_set = paper.setFinish();*/
 	});
 }//close window.onload()
 
@@ -102,4 +109,27 @@ function getViewport() {
 		viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
 	}
 	return [viewPortWidth, viewPortHeight];
+}
+
+function getSvgCoordinates(event, paper) {
+	var mainsvg = paper.canvas;
+	alert(mainsvg);
+	var m = mainsvg.getScreenCTM();
+	var p = mainsvg.createSVGPoint();
+	var x, y;
+
+	x = event.pageX;
+	y = event.pageY;
+
+	p.x = x;
+	p.y = y;
+	p = p.matrixTransform(m.inverse());
+
+	x = p.x;
+	y = p.y;
+
+	x = parseFloat(x.toFixed(3));
+	y = parseFloat(y.toFixed(3));
+
+	return {x: x, y: y};
 }
