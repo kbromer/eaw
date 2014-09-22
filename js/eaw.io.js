af07faa87920a812abd5acfcb226054358bfdf41
Kevin Bromer <kbromer@salesforce.com>
2014-09-22 01:06:02 -0400

    io

--- js/eaw.io.js
+++ js/eaw.io.js
@@ -3,6 +3,7 @@
   eaw.io.connectToServer = function () {
     //connect socket
     eaw.io.socket = io.connect();
+
     eaw.io.socket.on('onconnected', function( data ) {
       //Note that the data is the object we sent from the server, as is. So we can assume its id exists.
       console.log( 'Connected successfully to the socket.io server. My server side ID is ' + data.id );
